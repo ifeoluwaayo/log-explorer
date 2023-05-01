2b0c7cf7f54197c822897c1ca9d1f98c8eeef160
ifeoluwaayo <adeayomide112@gmail.com>
2023-05-01 18:52:56 +0100

    okay

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,6 +7,19 @@ export const Table = ({ logs }) => {
 				{logs.map((log, index) => (
 					<Log key={index} log={log} />
 				))}
+
+				{logs.length === 0 && (
+					<div>
+						<div className="w-full h-[80px] animate-pulse relative">
+							<div className="bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 absolute left-0 top-0 bottom-0 right-0 opacity-50 rounded-md"></div>
+							<div className="bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 absolute left-0 top-0 bottom-0 right-0 opacity-50 rounded-md animate-shimmer"></div>
+						</div>
+						<div className="w-full h-[80px] animate-pulse relative mt-[1px]">
+							<div className="bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 absolute left-0 top-0 bottom-0 right-0 opacity-50 rounded-md"></div>
+							<div className="bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 absolute left-0 top-0 bottom-0 right-0 opacity-50 rounded-md animate-shimmer"></div>
+						</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
